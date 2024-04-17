7e747229f645e6b06aaf7619bce4a7df09d25c38
Jean-Emmanuel87 <54609010+Jean-Emmanuel87@users.noreply.github.com>
2024-04-17 10:44:54 +0900

    Add files via upload

---
+++ path_modeling.m
@@ -0,0 +1,23 @@
+% Initialize the current directory
+currentDir = pwd;
+
+% Loop to move up in the directory structure until you find the 'code_for_publication' directory or reach the root
+while true
+    % Check if the 'main' directory exists in the current directory
+    codeDir = fullfile(currentDir, 'main_modeling');
+    if exist(codeDir, 'dir')
+        cd(codeDir);
+        disp(['Changed directory to: ' codeDir]);
+        break;
+    else
+        % Check if the current directory is 'code_for_publication'
+        [parentDir, currentFolderName, ~] = fileparts(currentDir);
+        if strcmp(currentFolderName, 'code_for_publication')
+            error(['The directory ' codeDir ' does not exist.']);
+        elseif isempty(parentDir) || strcmp(currentDir, parentDir)  % Check if we've reached the root
+            error('Reached the root directory without finding ''code_for_publication''.');
+        end
+        % Update currentDir to move one level up
+        currentDir = parentDir;
+    end
+end
